e1b4ee961174afd2bb314c843081b0eeda5d82c8
Scott Vickers <scott.w.vickers@gmail.com>
2020-01-26 21:13:13 -0800

    fix styling bug with extra gatsby-ssr config file

--- gatsby-ssr.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ThemeWrapper } from './plugins/gatsby-plugin-mui-theme-provider/theme-wrapper';
-
-export const wrapRootElement = ({ element }) => {
-  return <ThemeWrapper>{element}</ThemeWrapper>;
-};
