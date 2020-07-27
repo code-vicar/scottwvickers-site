1c6a48fc77c167dfb92f7d8b9ddbfa0e67465fdc
Scott Vickers <scott.w.vickers@gmail.com>
2020-07-26 22:32:41 -0700

    guard against window access for gatsby ssr

--- src/utils/menuStorage.ts
+++ src/utils/menuStorage.ts
@@ -7,14 +7,17 @@ const serializeMenu: (menu: readonly IMenuItem[]) => string = menu => {
 const MenuStorageKey = 'menu'
 
 export const saveMenu: (menu: readonly IMenuItem[]) => void = menu => {
-  const serializedMenu = serializeMenu(menu)
-  if (!window) {
+  if (typeof window === 'undefined') {
     return
   }
+  const serializedMenu = serializeMenu(menu)
   window.localStorage.setItem(MenuStorageKey, serializedMenu)
 }
 
 export const loadMenu: () => readonly IMenuItem[] = () => {
+  if (typeof window === 'undefined') {
+    return []
+  }
   const serializedMenu = window.localStorage.getItem(MenuStorageKey)
   if (!serializedMenu) {
     return []
